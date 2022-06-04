e60c882ec76eeb045eca901864af910e29d8d4df
alejandro_deulofeu_fleites <alejandro.df@ncs.com.sg>
2022-06-04 12:13:31 +0700

    Clean structure

--- first sample/init_inventory.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "[all]" > inventory
-
-# Get the public IP addresses of ec2 instances in an aws account
-aws ec2 describe-instances \
-   --query 'Reservations[*].Instances[*].PublicIpAddress' \
-   --output text >> inventory
--- second_sample/run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash -e
-rm inventory
-./init_inventory.sh
-cat inventory
-ansible-playbook main-remote.yml -i inventory --private-key $1 -u ec2-user
